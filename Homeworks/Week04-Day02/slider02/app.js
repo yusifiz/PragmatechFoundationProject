7b4d69c7531584cb9b1e02b38547b5ff5bb98989
Yusif Osmanov <yusifosmanov475@gmail.com>
2021-08-04 03:05:24 +0400

    js slider

---
+++ Homeworks/Week04-Day02/slider02/app.js
@@ -0,0 +1,11 @@
+// var slider = document.getElementsByClassName("slider");
+// var cmn = document.getElementsByClassName("commonSlide");
+// var element = document.getElementsByClassName("sldr");
+
+// function salam(){
+//     alert("salam kele");
+// }
+
+// element.addEventListener('click', function(){
+//     alert("salam ");
+// });
