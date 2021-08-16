229022834f41c482a96bd61b4c7175acfc9441e2
Yusif Osmanov <yusifosmanov475@gmail.com>
2021-08-17 02:29:04 +0400

    Flask task: worker

--- Works/Python/Flask/app.py
+++ Works/Python/Flask/app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask,render_template
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates')
+
+workers=[
+    {
+        'ad':'Yusif',
+        'soyad':'Osmanov',
+        'email':'osmanov.yusif@gmail.com'
+    },
+    {
+        'ad':'Filankes',
+        'soyad':'Filankesov',
+        'email':"ff@gmail.com"
+    }
+]
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    return render_template('index.html')
+@app.route("/workers")
+def worker():
+    return render_template('workers.html',workers=workers)
+
+@app.route("/contact")
+def cont():
+    return render_template('contact.html')
 
 if __name__=="__main__":
     app.run(debug=True)
